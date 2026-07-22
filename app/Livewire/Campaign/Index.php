27447edf1e9e8a6b576a7df0d99cb91c9299b524
Josias Bueno <messias_jpb@hotmail.com>
2026-07-22 01:02:36 -0300

    Implement campaign listing with pagination and enhance empty state message

--- app/Livewire/Campaign/Index.php
+++ app/Livewire/Campaign/Index.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Campaign;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    
+    #[Computed]
+    public function campaigns()
+    {
+        return auth()->user()->campaigns()->latest()->paginate(10);
+    }
+
     public function render()
     {
         return view('livewire.campaign.index');
--- resources/views/livewire/campaign/index.blade.php
+++ resources/views/livewire/campaign/index.blade.php
@@ -6,8 +6,25 @@
         </div>
     </div>
     
-    <x-alert color="secondary" light icon="heart" title="Você ainda não tem campanhas">
-        Que tal criar sua primeira campanha? Clique no botão acima para começar!
-    </x-alert>
+    <div class="space-y-4">
+        @forelse ($this->campaigns as $campaign)
+            {{-- <livewire:campaign.card :campaign="$campaign" :wire:key="$campaign->id" /> --}}
+            <x-card>
+                <small class="text-gray-500">{{ $campaign->created_at->format('d/m/Y') }}</small>
+                <p>
+                    <a href="{{ route('campaigns.show', $campaign) }}">{{ $campaign->name }}</a>
+                </p>
+            </x-card>
+        @empty
+            <div class="col-span-full">
+                <x-alert color="secondary" light icon="heart" title="Você ainda não tem campanhas">
+                    Que tal criar sua primeira campanha? Clique no botão acima para começar!
+                </x-alert>
+            </div>
+        @endforelse
+        <div class="mt-4">
+            {{ $this->campaigns->links() }}
+        </div>
+    </div>
 
 </div>
