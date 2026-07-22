901baf83b46d573ee09801f266267766737085d3
Josias Bueno <messias_jpb@hotmail.com>
2026-07-21 22:33:12 -0300

    Changed primary, secondary and neutral colors. Created header css class.

--- resources/views/livewire/campaign/index.blade.php
+++ resources/views/livewire/campaign/index.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- An unexamined life is not worth living. - Socrates --}}
+    <div class="header">
+        <h1>Minhas campanhas</h1>
+        <div>
+            <x-button>Criar Campanha</x-button>
+        </div>
+    </div>
+    
+    <x-alert color="secondary" light icon="heart" title="Você ainda não tem campanhas">
+        Que tal criar sua primeira campanha? Clique no botão acima para começar!
+    </x-alert>
+
 </div>
